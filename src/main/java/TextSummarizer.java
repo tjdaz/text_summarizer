0597c4e78f310b88e934bce2affa37a422b2847a
Timothy James Duffy <tjdaz@email.arizona.edu>
2023-03-28 17:29:26 -0700

    Initial commit.

---
+++ src/main/java/TextSummarizer.java
@@ -0,0 +1,13 @@
+public class TextSummarizer {
+
+    /** TextSummarizer constructor. */
+    public TextSummarizer() {
+        buildIndex();
+    }
+
+    /** Builds the index. */
+    private void buildIndex() {
+        // Placeholder for the build index function.
+    }
+
+}
