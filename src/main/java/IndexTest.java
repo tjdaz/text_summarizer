cd81ecc52e78a258a4e318a46fbc36dab04298aa
Timothy James Duffy <tjdaz@email.arizona.edu>
2023-04-20 23:17:52 -0700

    TextSummarizer now builds indexes.

--- src/main/java/IndexTest.java
+++ src/main/java/IndexTest.java
@@ -1,43 +1,85 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MatchAllDocsQuery;
-import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 
 public class IndexTest {
     public static void main(String[] args) {
         try {
-            // Writes all the index to files, only need to do this once.
-            Index.buildIndex(Index.TEST_TRAIN_INDEX_PATH, Index.TEST_TRAIN_DATA_PATH);
-            Index.buildIndex(Index.TEST_TEST_INDEX_PATH, Index.TEST_TEST_DATA_PATH);
-            Index.buildIndex(Index.TEST_VAL_INDEX_PATH, Index.TEST_VAL_DATA_PATH);
+            // Run TextSummarizer.java first, the Lucene indexes must be built first.
+            // Open the /train, /test, and /val directories with Lucene.
+            FSDirectory trainDir = FSDirectory.open(new File(Index.TRAIN_INDEX_PATH).toPath());
+            FSDirectory testDir = FSDirectory.open(new File(Index.TEST_INDEX_PATH).toPath());
+            FSDirectory valDir = FSDirectory.open(new File(Index.VAL_INDEX_PATH).toPath());
 
-            // Open and search the test index for the test training data (small dataset).
-            FSDirectory directory = FSDirectory.open(new File(Index.TEST_TRAIN_INDEX_PATH).toPath());
-            IndexReader reader = DirectoryReader.open(directory);
-            IndexSearcher indexSearcher = new IndexSearcher(reader);
+            // Open a reader for each directory.
+            IndexReader trainReader = DirectoryReader.open(trainDir);
+            IndexReader testReader = DirectoryReader.open(testDir);
+            IndexReader valReader = DirectoryReader.open(valDir);
+
+            // Open a searcher for each directory.
+            IndexSearcher trainSearcher = new IndexSearcher(trainReader);
+            IndexSearcher testSearcher = new IndexSearcher(testReader);
+            IndexSearcher valSearcher = new IndexSearcher(valReader);
 
             // Return all the documents in the index to check output (not tokenized yet).
             MatchAllDocsQuery query = new MatchAllDocsQuery();
-            TopDocs topDocs = indexSearcher.search(query, reader.maxDoc());
 
-            // Print the fields of each document
-            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-                Document doc = reader.document(scoreDoc.doc);
-                System.out.println("ID: " + doc.get("id"));
-                System.out.println("Content: " + doc.get("article"));
-                System.out.println("Summary: " + doc.get("summary"));
-                System.out.println("------------------------------");
+            // Get all train, test, and val docs.
+            TopDocs trainTopDocs = trainSearcher.search(query, trainReader.maxDoc());
+            TopDocs testTopDocs = testSearcher.search(query, testReader.maxDoc());
+            TopDocs valTopDocs = valSearcher.search(query, valReader.maxDoc());
+
+            // Get the number of training docs, add it to total docs.
+            int trainDocsCount = trainTopDocs.scoreDocs.length;
+            int testDocsCount = testTopDocs.scoreDocs.length;
+            int valDocsCount = valTopDocs.scoreDocs.length;
+            int totalDocsCount = trainDocsCount + testDocsCount + valDocsCount;
+
+            // Print data about number of Documents in the index.
+            System.out.println("*****************************************************");
+            System.out.println("Number of total docs: " + totalDocsCount);
+            System.out.println("Number of train docs: " + trainDocsCount);
+            System.out.println("Number of test docs: " + testDocsCount);
+            System.out.println("Number of val docs: " + valDocsCount);
+            System.out.println("*****************************************************");
+
+            // Array of TopDocs from all indexes for looping.
+            TopDocs[] topDocs = { trainTopDocs, testTopDocs, valTopDocs };
+            IndexSearcher[] searcher = { trainSearcher, testSearcher, valSearcher };
+
+            // Number of random documents to display.
+            int randDocCount = 3;
+
+            // Test: Get three random articles from train index.
+            for (int i = 0; i < topDocs.length; i++) {
+                for (int j = 0; j < randDocCount; j++) {
+                    Random rand = new Random();
+                    int randomNum = rand.nextInt(topDocs[j].scoreDocs.length);
+                    Document doc = searcher[j].doc(topDocs[j].scoreDocs[randomNum].doc);
+                    System.out.println("ID: " + doc.get("id"));
+                    System.out.println("Content: " + doc.get("article"));
+                    System.out.println("Summary: " + doc.get("summary"));
+                    System.out.println("------------------------------");
+                }
             }
-            // Close the reader and directory.
-            reader.close();
-            directory.close();
+
+            // Close the readers and directories.
+            trainReader.close();
+            testReader.close();
+            valReader.close();
+            trainDir.close();
+            testDir.close();
+            valDir.close();
+
+            // Catch all IOExceptoins and print stack trace.
         } catch (IOException e) {
             e.printStackTrace();
         }
